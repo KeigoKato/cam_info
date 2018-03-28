c164045556232c632137f82ba4d34c4dc288c15e
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-28 23:02:10 +0900

    [add] 画像のリサイズの設定をrmagickで追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  process :resize_to_limit => [740, 740]
+
+  process :convert => 'jpg'
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
 
   def extension_white_list
