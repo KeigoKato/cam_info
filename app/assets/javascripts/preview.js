f22dffeb64492cc1e93c4d2696e41b6ef49906da
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-31 14:22:13 +0900

    [upadte] ページ更新時にjsが動くようになった

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,8 +1,6 @@
-$(function(){
+$(document).on('turbolinks:load', function() { 
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  console.log("page was loaded")
   $('form').on('change', 'input[type="file"]', function(e) {
-    console.log("file was changed")
     var file = e.target.files[0],
         reader = new FileReader(),
         $preview = $("#preview");
