fc6546e5804998828261f71ef05c80988fdd7282
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-08 14:44:06 +0900

    [update] タグの体裁を整える

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Picture.tag_counts_on(:tags).order('count DESC')
+    @tags = Picture.tag_counts_on(:tags).order('count ASC')
   end
 
 end
