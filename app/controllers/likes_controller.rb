d38ff7163109b58cda06587bb2fe39ecf784f906
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-10 23:16:02 +0900

    [add] likesコントローラにdestroyアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,10 @@ class LikesController < ApplicationController
     redirect_to request.path
   end
 
+  def destroy
+    @likes = Like.find_by(user_id: current_user.id, picture_id: params[:picture_id])
+    @likes.destroy
+    redirect_to request.path
+  end
+
 end
