6242e567df8c8f9e21c758a524f010b06db23b8a
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-10 23:10:00 +0900

    [add] likesコントローラのcreateアクションを作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,9 @@
 class LikesController < ApplicationController
 
   def create
+    @likes = Like.new(user_id: current_user.id, picture_id: params[:picture_id])
+    @likes.save
+    redirect_to request.path
   end
 
 end
