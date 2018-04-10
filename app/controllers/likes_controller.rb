b03b919851e6f95c496c212ca6bd8faaa3b6db2a
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-10 23:31:44 +0900

    [fix] いいねのリダイレクト先を一時的に削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
   def create
     @likes = Like.new(user_id: current_user.id, picture_id: params[:picture_id])
     @likes.save
-    redirect_to request.path
+    # redirect_to request.path
   end
 
   def destroy
     @likes = Like.find_by(user_id: current_user.id, picture_id: params[:picture_id])
     @likes.destroy
-    redirect_to request.path
+    # redirect_to request.path
   end
 
 end
