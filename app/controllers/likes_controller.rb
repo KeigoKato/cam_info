579c980975472e369f99a55db4ae4250f7ab0bd3
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-11 21:00:19 +0900

    [add] タグで検索が可能になった

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,20 @@ class LikesController < ApplicationController
     @likes = Like.new(user_id: current_user.id, picture_id: params[:picture_id])
     @likes.save
     # redirect_to request.path
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
     @likes = Like.find_by(user_id: current_user.id, picture_id: params[:picture_id])
     @likes.destroy
     # redirect_to request.path
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,11 @@ class TagsController < ApplicationController
 
   def index
     @tags = Picture.tag_counts_on(:tags).order('count DESC')
+    @pictures = Picture.tagged_with(params[:tag_name]).includes(:tags)
+  end
+
+  def show
+    @picture = Picture.tagged_with(params[:tag_name])
   end
 
 end
