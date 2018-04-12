b6f4b29b70f9e5f4f09655803e9941a6e4c2b742
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-12 19:59:42 +0900

    [add] いいねのajax通信に成功

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,30 @@
 class LikesController < ApplicationController
+  before_action :set_pictures
 
   def create
-    @likes = Like.new(user_id: current_user.id, picture_id: params[:picture_id])
-    @likes.save
-    # redirect_to request.path
+    likes = Like.new(user_id: current_user.id, picture_id: params[:picture_id])
     respond_to do |format|
-      format.html
-      format.js
+      if likes.save
+        format.html
+        format.js
+      end
     end
   end
 
   def destroy
-    @likes = Like.find_by(user_id: current_user.id, picture_id: params[:picture_id])
-    @likes.destroy
-    # redirect_to request.path
+    likes = Like.find_by(user_id: current_user.id, picture_id: params[:picture_id])
     respond_to do |format|
-      format.html
-      format.js
+      if likes.destroy
+        format.html
+        format.js
+      else
+        format.html
+      end
     end
+  end
+
+  def set_pictures
+    @pictures = Picture.order("created_at DESC").includes(:tags)
   end
 
 end
